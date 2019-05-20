76c364f40f9fffe6cf83132d034c62a4dfc2c576
nick olmsted <go_go_loss@hotmail.com>
2019-05-20 14:37:02 -0700

    Add project files.

---
+++ UploadFileTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UploadFileTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
