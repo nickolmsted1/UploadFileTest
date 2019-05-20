5d8b2ee4a913b3ca07e874ed1d91cb56526c1a32
nick olmsted <go_go_loss@hotmail.com>
2019-05-20 15:18:14 -0700

    working on image upload. added some files to get working on image upload

--- UploadFileTest/Controllers/HomeController.cs
+++ UploadFileTest/Controllers/HomeController.cs
@@ -20,6 +20,32 @@ namespace UploadFileTest.Controllers
             return View();
         }
 
+        public IActionResult FileUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FileUpload(Image pic)
+        {
+            // https://stackoverflow.com/questions/47185920/upload-image-in-asp-net-core
+
+            
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                foreach (var image in files)
+                {
+                    if (image != null && image.Length > 0)
+                    {
+                        var file = image;
+                    }
+                }
+            }
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ UploadFileTest/Models/FileUpload.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UploadFileTest.Models
+{
+    public class Image
+    {
+        public IFormFile FileToUpload { get; set; }
+        public string FileName { get; set; }
+
+    }
+}
